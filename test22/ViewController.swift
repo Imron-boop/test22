d906a5831f9c357078bb601e38be50fe82dd320d
Amri <chozinul@gmail.com>
2019-11-30 11:15:14 +0700

    Initial Commit

---
+++ test22/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  test22
+//
+//  Created by muchamad amri on 30/11/19.
+//  Copyright © 2019 muchamad amri. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
